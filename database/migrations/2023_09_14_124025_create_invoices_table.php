632adbc4e3cb08389107d7cd53d1f8cf0245fa38
Baraa <baraa_official@outlook.com>
2023-10-18 23:15:05 +0300

    Some updates

--- database/migrations/2023_09_14_124025_create_invoices_table.php
+++ database/migrations/2023_09_14_124025_create_invoices_table.php
@@ -11,11 +11,21 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up() : void
     {
         Schema::create('invoices', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->foreignId('sub_category_id')->constrained('sub_categories')->onDelete('cascade');
+            $table->foreignId('unit_id')->constrained('units')->onDelete('cascade');
+            $table->foreignId('type_id')->constrained('types')->onDelete('cascade');
+            $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
+            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
+            $table->integer('total_items');
+            $table->integer('total_quantity');
+            $table->decimal('total_price');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -24,7 +34,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down() : void
     {
         Schema::dropIfExists('invoices');
     }
