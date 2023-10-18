b096fdd3d754f723f637cc11ef08805270773f03
Kareem-Tarek <kareemtarekpk@gmail.com>
2023-10-18 23:21:48 +0300

    commiting

---
+++ app/Models/NamePrice.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NamePrice extends Model
+{
+    use HasFactory;
+}
