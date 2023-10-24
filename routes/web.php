6a755ccdafeb4413f4523cfe969d988b6819a84e
Baraa <baraa_official@outlook.com>
2023-10-24 12:32:51 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,68 +25,27 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::fallback(function () {
-    return abort('404');
-});
+Route::prefix('/')->middleware(['auth'])->group(function () {
 
-//---------> START Dashboard routes
-Route::prefix('dashboard')->middleware(['auth'])->group(function () {
-    //---------> START Dashboard (home or index) routes
     Route::get('/', [HomeController::class, 'index'])->name('dashboard');
-    //---------> END Dashboard (home or index) routes
-
-    //---------> START User Profile routes
     Route::get('/user-profile/{id}', [UserProfileController::class, 'indexProfile'])->name('user-profile.index');
     Route::get('/user-profile/{id}/edit', [UserProfileController::class, 'editProfile'])->name('user-profile.edit');
     Route::patch('/user-profile/{id}', [UserProfileController::class, 'update'])->name('user-profile.updateInfo');
     Route::get('/user-profile/change-password/{id}', [UserProfileController::class, 'changePasswordView'])->name('user-profile.changePasswordView');
     Route::patch('/user-profile/change-password/{id}/submit', [UserProfileController::class, 'changePassword'])->name('user-profile.changePassword');
-    //---------> END User Profile routes
+
 
     Route::resource('categories', CategoryController::class)->except(['show']);
-
-
-
-    //---------> START Sub Category routes
     Route::resource('/subcategories', SubCategoryController::class);
-    //---------> END Sub Category routes
-
-    //---------> START Unit routes
     Route::resource('/units', UnitController::class);
-    //---------> END Unit routes
-
-    //---------> START Type routes
     Route::resource('/types', TypeController::class);
-    //---------> END Type routes
-
-    //---------> START Company routes
     Route::resource('/companies', CompanyController::class);
-    //---------> END Company routes
-
-    //---------> START Exported Product routes
     Route::resource('/exported-products', ExportedProductController::class);
-    //---------> END Exported Product routes
-
-    //---------> START Client routes
     Route::resource('/clients', ClientController::class);
-    //---------> END Client routes
-
-    //---------> START Product routes
     Route::resource('/products', ProductController::class);
     Route::get('/search', [ProductController::class, 'productsSearchResult'])->name('productsSearchResult.index');
-    //---------> END Product routes
-
-    //---------> START Price routes
     Route::resource('/prices', PriceController::class);
-    //---------> END Price routes
-
-    //---------> START Name Price routes
     Route::resource('/names-prices', NamePriceController::class);
-    //---------> END Name Price routes
-
-    //---------> START Invoice routes
     Route::resource('/invoices', InvoiceController::class);
-    //---------> END Invoice routes
 });
-//---------> END Dashboard routes
 
